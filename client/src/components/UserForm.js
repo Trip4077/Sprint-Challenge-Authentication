fa3fe3b22dd50a8b97d0fa63cef8c3a6d3a5efc5
Trip4077 <bj2336@email.vccs.edu>
2019-04-06 13:19:43 -0400

    UserForm Created, State Updates on Input

---
+++ client/src/components/UserForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from 'axios';
+
+class UserForm extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+          });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome</h1>
+
+                <label>Username</label>
+                <input type='text'
+                       name='username'
+                       placeholder='username'
+                       value={ this.state.username }
+                       onChange={ this.handleChange }
+                       />
+
+                <label>Password</label>
+                <input type='text'
+                       name='password'
+                       placeholder='password'
+                       value={ this.state.password }
+                       onChange={ this.handleChange }
+                       />
+
+                <button>Login</button>
+                <button>Register</button>
+            </div>
+        )
+    }
+}
+
+export default UserForm;
